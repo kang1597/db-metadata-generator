b0166fac36be8e90b58d221b8691d543a7ca00a8
kya <35wkd2817@gmail.com>
2026-08-28 11:41:55 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/portfolio/dbmetadatagenerator/DbMetadataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.dbmetadatagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMetadataGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbMetadataGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/dbmetadatagenerator/DbMetadataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.dbmetadatagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMetadataGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
